ec7a343ca4bcbba90752af2c137e8ffb2134fa33
fcalife <fcalife@gmail.com>
2019-08-12 07:33:13 -0300

    Initial setup, part #1

---
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,40 @@
+-- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
+require('kingdom_wars')
+
+-- Libraries and startup files
+require('libraries/timers')
+require('libraries/playertables')
+require('internal/gamesetup')
+require('internal/events')
+require('internal/util')
+require('events')
+
+-- Kingdom files
+--require('')
+
+function Precache(context)
+
+	-- Gamemode loading precache
+	print("Performing pre-load precache")
+
+	-- Examples
+	-- PrecacheResource("particle", "particles/dev/library/base_dust_hit_detail.vpcf", context)
+	-- PrecacheResource("particle_folder", "particles/test_particle", context)
+	-- PrecacheResource("model", "particles/heroes/viper/viper.vmdl", context)
+	-- PrecacheResource("model_folder", "particles/heroes/antimage", context)
+	-- PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_nevermore.vsndevts", context)
+	-- PrecacheUnitByNameSync("npc_dota_hero_ancient_apparition", context)
+	-- PrecacheItemByNameSync("example_ability", context)
+
+	-- Core sounds
+	PrecacheResource("soundfile", "soundevents/kingdom_soundevents.vsndevts", context)
+
+	-- We done
+	print("Finished pre-load precache")
+end
+
+-- Create the game mode when we activate
+function Activate()
+	GameRules.GameMode = GameMode()
+	GameRules.GameMode:_InitGameMode()
+end
